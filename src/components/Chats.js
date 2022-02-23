5a1c047da30eb1b313d0e21f4fc1837b0b669dde
nima <nimaqanbari1999@gmail.com>
2022-02-23 01:24:28 -0800

    add Chats and styles

---
+++ src/components/Chats.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+//styles
+import styles from "./Chats.module.css"
+
+const Chats = () => {
+  return (
+    <div className={styles.container}>
+       
+    </div>
+  )
+}
+
+export default Chats
