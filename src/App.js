23a2021ff1e69e75da1944064d0f1996ed1133fa
nima <nimaqanbari1999@gmail.com>
2022-02-23 01:22:23 -0800

    add AuthContextProvider

--- src/App.js
+++ src/App.js
@@ -3,13 +3,20 @@ import { Routes, Route } from "react-router-dom";
 
 //components
 import Login from "./components/Login";
+import Chats from "./components/Chats";
+
+//context
+import AuthContextProvider from "./Context/AuthContextProvider";
 
 const App = () => {
   return (
     <div>
-      <Routes>
-        <Route path="/"  element={<Login />}/>
-      </Routes>
+      <AuthContextProvider>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/chats" element={<Chats />} />
+        </Routes>
+      </AuthContextProvider>
     </div>
   );
 };
